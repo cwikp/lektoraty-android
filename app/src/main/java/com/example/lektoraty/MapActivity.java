e1cbda1341a09b2c4080f19a2d41d3914b392f78
Pawel Cwik <pawel.cwik@allegro.pl>
2019-06-04 17:52:26 +0200

    map example

---
+++ app/src/main/java/com/example/lektoraty/MapActivity.java
@@ -0,0 +1,74 @@
+package com.example.lektoraty;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.akexorcist.googledirection.DirectionCallback;
+import com.akexorcist.googledirection.GoogleDirection;
+import com.akexorcist.googledirection.model.Direction;
+import com.akexorcist.googledirection.util.DirectionConverter;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
+    private LatLng camera = new LatLng(50.062299, 19.923949);
+    private String serverKey = "";
+    private LatLng origin = new LatLng(50.062299, 19.923949);
+    private LatLng destination = new LatLng(50.064696, 19.926052);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) throws SecurityException {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_fragment);
+
+        ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        provideDirections();
+    }
+
+
+    private void provideDirections() {
+        this.googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(camera, 15));
+        GoogleDirection.withServerKey(serverKey)
+                .from(origin)
+                .to(destination)
+                .execute(new DirectionCallback() {
+                    @Override
+                    public void onDirectionSuccess(Direction direction, String rawBody) {
+                        System.out.println(">>Direction: " + direction);
+                        System.out.println(">>Body: " + rawBody);
+                        markDirections(direction);
+                    }
+
+                    @Override
+                    public void onDirectionFailure(Throwable t) {
+                        System.out.println(">>Error");
+                    }
+                });
+    }
+
+    private void markDirections(Direction direction) {
+        if (direction.isOK()) {
+            googleMap.addMarker(new MarkerOptions().position(origin));
+            googleMap.addMarker(new MarkerOptions().position(destination));
+            ArrayList<LatLng> directionPositionList = direction.getRouteList().get(0).getLegList().get(0).getDirectionPoint();
+            googleMap.addPolyline(DirectionConverter.createPolyline(this, directionPositionList, 5, Color.RED));
+        } else {
+            System.out.println(">>Direction not Ok");
+        }
+    }
+}
